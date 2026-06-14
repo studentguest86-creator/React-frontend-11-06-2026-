01fb75d32ef06c01514b213b33de8afdd77f9a22
studentguest86-creator <studentguest86@gmail.com>
2026-06-14 14:16:45 +0600

    homepage complete

---
+++ src/components/common/FeaturedProducts.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import ProductImg from '../../assets/Mens/two.jpg';
+
+
+const FeaturedProducts = () => {
+  return (
+    <section className="section-2 py-2">
+        <div className="container">
+            <h2>Featured Products</h2>
+            <div className="row mt-4">
+                <div className="col-md-3">
+                    <div className="product card border-0">
+                    <div className="card-img">
+                        <img src={ProductImg} alt="" className='w-100' />
+                    </div>
+                    <div className="card-body pt-3">
+                        <a href="">Red check shirt for Men</a>
+                        <div className="price">
+                        $50 <span className='text-decoration-line-through'>$80</span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="product card border-0">
+                    <div className="card-img">
+                        <img src={ProductImg} alt="" className='w-100' />
+                    </div>
+                    <div className="card-body pt-3">
+                        <a href="">Red check shirt for Men</a>
+                        <div className="price">
+                        $50 <span className='text-decoration-line-through'>$80</span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="product card border-0">
+                    <div className="card-img">
+                        <img src={ProductImg} alt="" className='w-100' />
+                    </div>
+                    <div className="card-body pt-3">
+                        <a href="">Red check shirt for Men</a>
+                        <div className="price">
+                        $50 <span className='text-decoration-line-through'>$80</span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-md-3">
+                    <div className="product card border-0">
+                    <div className="card-img">
+                        <img src={ProductImg} alt="" className='w-100' />
+                    </div>
+                    <div className="card-body pt-3">
+                        <a href="">Red check shirt for Men</a>
+                        <div className="price">
+                        $50 <span className='text-decoration-line-through'>$80</span>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default FeaturedProducts
